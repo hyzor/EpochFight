fbfc970f6f5bc2582500b37f31be5821d2e4d379
Jesper Hansson Falkenby <jesper.hansson.f@gmail.com>
2017-02-15 14:22:24 +0100

    Added simple camera controls (WASD and arrow keys). Also added an "intro camera script"--feel free to try it on the main camera!

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+    public float moveSpeed = 1.0f;
+    public float turnSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(new Vector3(0.0f, moveSpeed * Time.deltaTime, 0.0f));
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.Translate(new Vector3(0.0f, -(moveSpeed * Time.deltaTime), 0.0f));
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Translate(new Vector3(moveSpeed * Time.deltaTime, 0.0f, 0.0f));
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Translate(new Vector3(-(moveSpeed * Time.deltaTime), 0.0f, 0.0f));
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(new Vector3(0.0f, 0.0f, moveSpeed * Time.deltaTime));
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(new Vector3(0.0f, 0.0f, -(moveSpeed * Time.deltaTime)));
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.Rotate(new Vector3(0.0f, -(turnSpeed * Time.deltaTime), 0.0f));
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.Rotate(new Vector3(0.0f, turnSpeed * Time.deltaTime, 0.0f));
+        }
+    }
+}
---
+++ Assets/Scripts/IntroCameraScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroCameraScript : MonoBehaviour {
+    public float pingPongDuration = 10.0f;
+    public float zEndOffset;
+    public float zStartOffset;
+
+    public float yRotStart = -45.0f;
+    public float yRotEnd = 20.0f;
+    public float xRotationAngle = 45.0f;
+
+    public float rotSmoothing = 2.0f;
+
+    private float zStart;
+    private float zEnd;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.zStart = this.gameObject.transform.position.z + zStartOffset;
+        this.zEnd = this.gameObject.transform.position.z + zEndOffset;
+        //this.zEnd = zStart + 15.0f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float pingPong = Mathf.PingPong(Time.time, pingPongDuration) / pingPongDuration;
+        Transform trans = this.gameObject.transform;
+        float zLerp = Mathf.Lerp(zStart, zEnd, pingPong);
+        float rotYLerp = Mathf.Lerp(yRotStart, yRotEnd, pingPong);
+        Vector3 newPos = new Vector3(trans.position.x, trans.position.y, zLerp);
+        Quaternion targetRotQuat = Quaternion.Euler(xRotationAngle, rotYLerp, trans.rotation.z);
+        Quaternion quatSlerp = Quaternion.Slerp(trans.rotation, targetRotQuat, Time.deltaTime * rotSmoothing);
+        trans.position = newPos;
+        trans.rotation = quatSlerp;
+    }
+}
