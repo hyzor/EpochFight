afbb94f4c1a47dccaa945cc2c5c05b2b782c5ade
Jesper Hansson Falkenby <jesper.hansson.f@gmail.com>
2017-02-20 19:18:31 +0100

    Begin to integrate WorkerExample into Demo_Environment

---
+++ Assets/Scripts/FollowMainCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMainCamera : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.rotation = Camera.main.transform.rotation;
+        transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z);
+	}
+}
