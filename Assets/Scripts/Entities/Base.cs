00b3c58551d287c65fafbf3673e046e19440e51a
Jesper Hansson Falkenby <jesper.hansson.f@gmail.com>
2017-02-21 12:27:58 +0100

    Fix Base destruction.

--- Assets/Scripts/Entities/Base.cs
+++ Assets/Scripts/Entities/Base.cs
@@ -6,17 +6,22 @@ using UnityEngine.EventSystems;
 public class Base : MonoBehaviour, IClickable
 {
     private MouseListener mouseListener;
+    private Entity entity;
 
 	// Use this for initialization
 	void Start ()
     {
         mouseListener = GameObject.Find("MouseListener").GetComponent<MouseListener>();
+        entity = gameObject.GetComponent<Entity>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+        if (entity.deathTrigger && !entity.flaggedForRemoval)
+        {
+            entity.flaggedForRemoval = true;
+        }
 	}
 
     public void OnLeftClick()
