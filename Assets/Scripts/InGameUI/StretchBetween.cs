c2486a3e43723a6dc4bf73735cb9942ba4a4f702
JonathanBeck <jonathan.beck@king.com>
2017-02-24 15:45:33 +0100

    drag n drop ordering

---
+++ Assets/Scripts/InGameUI/StretchBetween.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StretchBetween : MonoBehaviour {
+
+	public Vector3 origin;
+	public Vector3 target;
+
+	public void Start () {
+	}
+
+	public void Update () {
+		transform.localScale = new Vector3(
+			transform.localScale.x,
+			transform.localScale.y,
+			Vector3.Distance(origin,target));
+		transform.position = (origin + target)/2;	
+		transform.LookAt(target);
+	}
+}
